b2c888aa0019ed1631a70909bd1304da3f30cdf5
John Bretherton <270022735@bun2-206-10.local>
2019-06-26 11:55:00 +1200

    Getting Kia Ora pop up to appear

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
-console.log('js is loading');
+var motorbike = {
+    peopleMin: 1,
+    peopleMax: 1,
+    dailyRate: 109,
+    minimumDays: 1,
+    maximumDays: 5,
+    gasRate: 3.7
+};
+
+var smallCar = {
+    peopleMin: 1,
+    peopleMax: 2,
+    dailyRate: 129,
+    minimumDays: 1,
+    maximumDays: 10,
+    gasRate: 8.5
+};
+
+var largeCar = {
+    peopleMin: 1,
+    peopleMax: 5,
+    dailyRate: 144,
+    minimumDays: 3,
+    maximumDays: 10,
+    gasRate: 9.7
+};
+
+var motorhome = {
+    peopleMin: 2,
+    peopleMax: 6,
+    dailyRate: 200,
+    minimumDays: 2,
+    maximumDays: 15,
+    gasRate: 17
+};
+
+$(document).ready(function(){
+    $(".letsGoBox").click(function(){
+        $(".mainContainer").hide();
+        $(".helpIconBox").show();
+        $(".kiaOraBox").show();
+    });
+});
