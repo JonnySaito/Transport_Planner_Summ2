df64ec707032add50a9d1133cbdcad03847f6383
John Bretherton <270022735@bun2-206-10.local>
2019-06-28 12:09:01 +1200

    Getting page show and hide functionality working

--- js/script.js
+++ js/script.js
@@ -1,60 +1,116 @@
-var motorbike = {
-    peopleMin: 1,
-    peopleMax: 1,
-    dailyRate: 109,
-    minimumDays: 1,
-    maximumDays: 5,
-    gasRate: 3.7
-};
 
-var smallCar = {
-    peopleMin: 1,
-    peopleMax: 2,
-    dailyRate: 129,
-    minimumDays: 1,
-    maximumDays: 10,
-    gasRate: 8.5
-};
+var vehicles = [
+    {
+        type: 'motorbike',
+        peopleMin: 1,
+        peopleMax: 2,
+        dailyRate: 129,
+        daysMin: 1,
+        daysMax: 10,
+        gasRate: 8.5
+    },
+    {
+        type: 'smallCar',
+        peopleMin: 1,
+        peopleMax: 2,
+        dailyRate: 129,
+        daysMin: 1,
+        daysMax: 10,
+        gasRate: 8.5
+    },
+    {
+        type: 'largeCar',
+        peopleMin: 1,
+        peopleMax: 5,
+        dailyRate: 144,
+        daysMin: 3,
+        daysMax: 10,
+        gasRate: 9.7
+    },
+    {
+        type: 'motorhome',
+        peopleMin: 2,
+        peopleMax: 6,
+        dailyRate: 200,
+        daysMin: 2,
+        daysMax: 15,
+        gasRate: 17
+    },
+];
+// GLOBAL VARIABLES
+var height;
+var width;
+var element;
+var currentPageElement;
+var numberPeople;
+var numberDays;
 
-var largeCar = {
-    peopleMin: 1,
-    peopleMax: 5,
-    dailyRate: 144,
-    minimumDays: 3,
-    maximumDays: 10,
-    gasRate: 9.7
-};
 
-var motorhome = {
-    peopleMin: 2,
-    peopleMax: 6,
-    dailyRate: 200,
-    minimumDays: 2,
-    maximumDays: 15,
-    gasRate: 17
-};
 
+// PAGE LOADING + CLICK TO START
 $(document).ready(function(){
-    // var currentPage = $("#page1");
-
     $(".letsGoBox").click(function(){
-        $(".mainContainer").hide();
+        getHeight();
+        currentPageElement = $("#page2");
+        $("#page1").hide();
         $(".helpIconBox").show();
-        $(".kiaOraBox").show();
-        // currentPage = $("#page"+1); ???
-        $(".goBackIcon").click(function(){
-            $(".helpIconBox").hide();
-            $(".kiaOraBox").hide();
-            $(".mainContainer").show();
-        });
-        $(".helpIconBox").click(function(){
-            $(".mainContainer").hide();
-            $(".kiaOraBox").hide();
-            $(".helpContainer").show();
-            $(".closeButton").click(function(){
-                $(".helpContainer").hide();
-                $(".kiaOraBox").show();
-            })
-        });
+        $("#page2").fadeIn(1000);
+        getHeightForNextPage();
     });
+// CLICK BACK ARROW TO RETURN TO PAGE 1
+    $(".goBackIcon").click(function(){
+        getHeight();
+        $("#page2").hide();
+        $(".helpIconBox").hide();
+        $("#page1").fadeIn(1000);
+        getHeightForNextPage();
+    });
+// CLICK ? ICON TO GO TO PAGE 3 (HELP PAGE)
+    $(".helpIconBox").click(function(){
+        getHeight();
+        currentPageElement.hide();
+        $(".helpIconBox").show();
+        $("#helpContainer").fadeIn(1000);
+        getHeightForNextPage();
+    });
+// CLICK X ICON TO RETURN TO PAGE 2 (KIA ORA PAGE)
+    $(".closeButton").click(function(){
+        $("#helpContainer").hide();
+        currentPageElement.fadeIn(1000);
+    });
+//ENTER NUMBER OF PEOPLE THEN PROCEED TO PAGE 4 (NUMBER OF DAYS)
+
+        $("#peopleOK").click(function(){
+            // console.log("clicked people OK button");
+            numberPeople = parseInt($("#people").val());
+            console.log(numberPeople);
+        });
+        // numberPeople = parseInt($("#people").val());
+        // console.log(numberPeople);
+        // if(numberPeople < 1 || numberPeople > 6) {
+        //     // tell user to input a number 1-6
+        //     $()
+        // }
+
+
+// GET HEIGHT & WIDTH OF CURRENT PAGE
+    function getHeight(){
+        element = $(".pageContainer");
+        height = element.outerHeight();
+        width = element.outerWidth();
+        element.hide();
+    }
+// GET HEIGHT & WIDTH OF NEXT PAGE
+    function getHeightForNextPage(){
+        var height2 = element.outerHeight();
+        var width2 = element.outerWidth();
+        element.css({height: height, width: width});
+        element.show();
+        element.animate({height: height2, width: width2}, 500);
+        setTimeout(function(){
+            element.css({height: '', width: ''});
+        }, 1000);
+    }
 });
+
+// var numberPeople = $("#people").val();
